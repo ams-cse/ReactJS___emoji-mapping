52e81aaf2ccfaf2c8344f1234842a063e2f41b89
Ahesanul Mahbub Shovon <shovonbd321@gmail.com>
2020-12-22 18:12:50 +0600

    initial

---
+++ src/emojipedia.js
@@ -0,0 +1,52 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: '🍓',
+    name: 'Strawberry',
+    meaning: 'Something or someone is extremely sweet! The strawberry stands for sunny days and summer holidays. (Together with champagne, flowers and chocolate) symbol of sensuality as well as romance and wonderful hours for a couple.'
+  },
+  {
+    id: 5,
+    emoji: '🥦',
+    name: 'Broccoli',
+    meaning: 'Childhood trauma, healthy lifestyle or vegan diet. Related to the cauliflower, it contains few calories and valuable ingredients. Green, healthy but not very popular.'
+  },
+  {
+    id: 6,
+    emoji: '🍅',
+    name: 'Tomato',
+    meaning: 'I like BLT sandwiches (bacon, lettuce and tomato)! or “I drink tomato juice - in Bloody Mary!“ In earlier times, the audience threw rotten tomatoes at actors whom they did not like.'
+  },
+  {
+    id: 7,
+    emoji: '🍕',
+    name: 'Slice Of Pizza',
+    meaning: 'Pizza salami is the most popular pizza in Germany. Many people associate it with a cozy evening with friends. Or to fight a hangover after an evening of convivial drinking.'
+  }
+
+];
+
+export default emojipedia;
+
+
